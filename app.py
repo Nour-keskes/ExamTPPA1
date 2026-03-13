77b96141bd764925f11b2b49229c206cb361efff
Nouvelle Identité <nour@email.com>
2026-03-13 03:45:02 +0100

    ajout calcul max

--- app.py
+++ app.py
@@ -1,2 +1,3 @@
 data=[2,4,6] print(sum(date))
 data=[2,4,6] print(somme:,sum(date)) print(Moyenne:,sum(date)/len(data))
+data=[2,4,6] print(somme:,sum(date)) print(Moyenne:,sum(date)/len(data)) print(Max:,max(data))
